7c407dd2c0b3e60376157e01e4e5ca755d41ccb7
christopher floess <skeptikos@gmail.com>
2015-08-21 16:54:15 +0200

    Implement functions capable of working with cached values

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Implementation of functions exibiting caching of difficult to
+## calculate values. This is done by using to <<- operator to save
+## values calculated in nested functions to the parent function's
+## scope, allowing other nested values to work with these values.
 
-## Write a short comment describing this function
 
+## This function takes a matrix as an argument. I returns an "object"
+## that provides a series of functions for retrieving and setting the
+## matrix and it's inverse. It does not however calculate its own
+## inverse. The set() function is also responsible for setting the
+## inverse of the matrix to null. This is done so that other functions
+## working with these "objects" whether to set the inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    get <- function(){
+        x
+    }
+    getInverse <- function(){
+        inverse
+    }
+    set <- function(y){
+        x <<- y
+        inverse <<- NULL
+    }
+    setInverse <- function(m) inverse <<- m
+    list(get = get, getInverse = getInverse, set = set, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function receives a matrix "object" and is responsible
+## computing the inverse of the "objects's" matrix. It ONLY does this
+## however, if the inverse of the "object's" matrix is null.
+cacheSolve <- function(x) {
+    i <- x$getInverse()
+    if(is.null(i)) {
+        i <<- solve(x$get())
+    }
+    i
 }
